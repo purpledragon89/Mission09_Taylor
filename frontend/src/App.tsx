2f13fb1e4d810210a41d6cc94d2b93abf155e951
purpledragon89 <rileywtaylor@gmail.com>
2025-03-05 09:36:43 -0700

    final

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,50 @@
 import './App.css';
-function Welcome()
-{
-  return <h1>Welcome to the NCAA teams directory!</h1>
+import data from '/Users/rileytaylor/Desktop/Mission09_Taylor/frontend/src/CollegeBasketballTeams.json';
+
+function Welcome() {
+  return <h1>Welcome to the NCAA teams directory!</h1>;
 }
 
-function Team(
-  {
-    school,
-    name,
-    city,
-    state
-  }:
-  {
-    school: string;
-    name: string;
-    city: string;
-    state: string;
-  }
-){
-  return
-  (
+function Team({
+  school,
+  name,
+  city,
+  state,
+}: {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}) {
+  return (
     <>
-    <h2>{school}</h2>
-    <h2>Home of the: {name}</h2>
-    <h2>Location: {city}, {state}</h2>
+      <h2>{school}</h2>
+      <h3>Home of the: {name}</h3>
+      <h4>
+        Location: {city}, {state}
+      </h4>
+      <hr />
     </>
   );
 }
 
-function TeamList()
-{
-  return
-  (
+function TeamList() {
+  return (
     <>
-    {TeamNames.map((singleteam)=>
-    (<Team {...singleteam} />
+      {data.teams.map((team) => (
+        <div key={team.tid}>
+          {team.school} - {team.name}
+        </div>
       ))}
     </>
-  )
+  );
 }
 
 function App() {
-  
   return (
     <>
-    <Welcome />
-    
+      <Welcome />
+      <TeamList />
     </>
   );
 }
