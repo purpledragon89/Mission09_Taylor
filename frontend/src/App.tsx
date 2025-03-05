be1a9b8e50ba67d917678377694f4a35bd2e8962
purpledragon89 <rileywtaylor@gmail.com>
2025-03-05 09:36:49 -0700

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,14 +17,14 @@ function Team({
   state: string;
 }) {
   return (
-    <>
+    <div>
       <h2>{school}</h2>
       <h3>Home of the: {name}</h3>
       <h4>
         Location: {city}, {state}
       </h4>
       <hr />
-    </>
+    </div>
   );
 }
 
@@ -32,9 +32,13 @@ function TeamList() {
   return (
     <>
       {data.teams.map((team) => (
-        <div key={team.tid}>
-          {team.school} - {team.name}
-        </div>
+        <Team
+          key={team.tid}
+          school={team.school}
+          name={team.name}
+          city={team.city}
+          state={team.state}
+        />
       ))}
     </>
   );
@@ -48,3 +52,5 @@ function App() {
     </>
   );
 }
+
+export default App;
